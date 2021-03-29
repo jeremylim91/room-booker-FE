306b29b3b5932d8a86d21e710cffcecef107cdf0
jeremylim91 <jeremylim_91@live.com>
2021-03-29 19:28:31 +0800

    created banner for homepage

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import Amenities from './Amenities.jsx'
+import Banner from './Banner.jsx'
+import Button from 'react-bootstrap/Button'
+import {Link} from 'react-router-dom'
+
 
 export default function HomePage() {
   return (
   <>
+
+    <Banner
+    title="Best-In-Class Rooms"
+    subtitle= "“This is the best meeting room I've ever been in”"
+    >
+    <Link to="/createBooking" className="btn-primary"><Button> BOOK NOW </Button></Link>
+      
+    </Banner>
     <Amenities/>
   </>
   )
