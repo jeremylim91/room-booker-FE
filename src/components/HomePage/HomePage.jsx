d3d0e8e83d4fd03ec4a46a143c1a5936d71e9f52
jeremylim91 <jeremylim_91@live.com>
2021-03-30 08:40:22 +0800

    created Hero to wrap banner on homepage

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Amenities from './Amenities.jsx'
+import Hero from './Hero.jsx'
 import Banner from './Banner.jsx'
 import Button from 'react-bootstrap/Button'
 import {Link} from 'react-router-dom'
@@ -9,14 +10,15 @@ import '../../styles/HomePage.scss'
 export default function HomePage() {
   return (
   <>
-
-    <Banner
-    title="Best-In-Class Rooms"
-    subtitle= "“This is the best meeting room I've ever been in”"
-    >
-    <Link to="/createBooking" className="btn-primary"><Button> BOOK NOW </Button></Link>
-      
-    </Banner>
+    <Hero>
+      <Banner
+      title="Best-In-Class Rooms"
+      subtitle= "“This is the best meeting room I've ever been in”"
+      >
+      <Link to="/createBooking" className="btn-primary"><Button> BOOK NOW </Button></Link>
+        
+      </Banner>
+    </Hero>
     <Amenities/>
   </>
   )
