56b5aaf519b6b6cea07bf9e2c0266c1370171beb
jeremylim91 <jeremylim_91@live.com>
2021-04-02 09:51:33 +0800

    updated code to allow user to pick room by click on img

--- src/components/CreateBookingMultistepForm/SelectRoom.jsx
+++ src/components/CreateBookingMultistepForm/SelectRoom.jsx
@@ -3,6 +3,7 @@ import {
   CreateBookingContext,
   CREATE_BOOKING_FORM,
   getRoomCatalogue,
+  updateSelectedRoom,
 } from '../../createBookingStore';
 import {formModes} from '../../createBookingStore.jsx';
 import Button from 'react-bootstrap/Button';
@@ -10,6 +11,7 @@ import Button from 'react-bootstrap/Button';
 import {writeStorage, deleteFromStorage} from '@rehooks/local-storage';
 
 export default function SelectRoom({setMode}) {
+  // perfrom destructuring on importated vars
   const {
     formStore,
     dispatchBookingForm,
@@ -18,21 +20,47 @@ export default function SelectRoom({setMode}) {
   } = useContext(CreateBookingContext);
   const {SELECT_DATE_TIME, FORM_STEP} = formModes;
 
+  // set a state comprised of rooms to display to user
   const [roomCatalogue, setRoomCatalogue] = useState([]);
+
+  // (No need for now): set a var for storing the selected room in local storage (to enable persistence)
+  const ROOM_ID = 'ROOM_ID';
+
+  // if there isn't storage for form info to persist, create it:
+  if (!formLocalStorage) {
+    writeStorage(CREATE_BOOKING_FORM);
+  }
 
   // on page load, query the db to get data on the available rooms
   useEffect(() => {
     getRoomCatalogue(setRoomCatalogue);
   }, []);
 
+  const handleRoomSelection = (roomId) => {
+    dispatchBookingForm(updateSelectedRoom(roomId));
+    // add the selected rm to local storage
+    //  there doesn't seem to be a need to store the cal events in local storage for now
+    writeStorage(CREATE_BOOKING_FORM, {...formLocalStorage, [ROOM_ID]: roomId});
+  };
+
   const DisplayRooms = () => {
-    console.log(`roomCatalogue is:`);
-    console.log(roomCatalogue);
     return roomCatalogue.map((room, idx) => {
       return (
         <div className="col-4">
-          <img src={`${room.thumbnail}`} alt="respective room"></img>
-          <p>{room.room_name}</p>
+          <h2>{room.room_name}</h2>
+          <p>Capacity: {room.max_occupancy}</p>
+          <button
+            type="submit"
+            onClick={(e) => {
+              // the selected room is in the storage, read from storage
+              handleRoomSelection(room.id);
+            }}
+            style={{border: 'none'}}>
+            <img
+              src={`${room.thumbnail}`}
+              alt="respective room"
+              style={{maxWidth: '100%'}}></img>
+          </button>
         </div>
       );
     });
