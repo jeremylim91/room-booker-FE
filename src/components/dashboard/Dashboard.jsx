59b02b5eaf340f961c63cf3cb0a01afd0c4607c4
jeremylim91 <jeremylim_91@live.com>
2021-04-04 09:40:06 +0800

    created skeleton for dashboard page

---
+++ src/components/dashboard/Dashboard.jsx
@@ -0,0 +1,122 @@
+import React, {useContext, useState, useEffect} from 'react';
+import CreateBookingModal from './CreateBookingModal.jsx';
+import Button from 'react-bootstrap/Button';
+import {writeStorage, deleteFromStorage} from '@rehooks/local-storage';
+import {
+  CreateBookingContext,
+  CREATE_BOOKING_FORM,
+  getAllEvents,
+  formModes,
+  updateMeetingStartEndAction,
+} from '../../createBookingStore';
+// import {formModes} from '../../createBookingStore.jsx';
+import {Calendar, momentLocalizer, Views} from 'react-big-calendar';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import moment from 'moment';
+import '../../styles/calendarStyles.scss';
+import {
+  getLowerBoundDate,
+  getUpperBoundDate,
+} from '../../utils/calendarRelatedFns.mjs';
+
+// Setup the localizer by providing the moment (or globalize) Object
+// to the correct localizer.
+const localizer = momentLocalizer(moment);
+
+export default function Dashboard() {
+  // destructure imported vars
+  const {
+    formStore,
+    dispatchBookingForm,
+    handleOnChange,
+    formLocalStorage,
+  } = useContext(CreateBookingContext);
+
+  // logic that tells app to get roomId if alr in local storage, else get it from global state
+  const roomId = formLocalStorage.roomId
+    ? formLocalStorage.roomId
+    : formStore.roomId;
+
+  const {SELECT_DATE_TIME, FORM_STEP} = formModes;
+
+  // ==============local states=====================
+  // manage modal display
+  const [show, setShow] = useState(false);
+  // display all meetings taking place in the room specified by user
+  const [allEvents, setAllEvents] = useState([]);
+  // Meeting details that the user inputs
+  const [userSelectionDetails, setuserSelectionDetails] = useState({});
+  // ==================================================
+
+  // =======set states to manage modal open/close======
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  // =================================================
+
+  useEffect(() => {
+    getAllEvents(setAllEvents, userId);
+    // for now, doesn't seem to be a need for the dates to persist (takes up local storage unecessarily)
+    // writeStorage(CREATE_BOOKING_FORM, {
+    //   ...formLocalStorage,
+    //   [CAL_EVENTS_BY_ROOM_ID]: allEvents,
+    // });
+  }, []);
+  const handleNextPage = () => {
+    setMode(SELECT_DATE_TIME);
+    writeStorage(FORM_STEP, SELECT_DATE_TIME);
+  };
+
+  const handleCancelForm = () => {
+    window.location = '/';
+    deleteFromStorage(CREATE_BOOKING_FORM);
+    deleteFromStorage(FORM_STEP);
+  };
+
+  // this fn runs whenever a user selects a timeslot on the calendar
+  const handleCreateEvent = (e) => {
+    console.log(e.start);
+    dispatchBookingForm(updateMeetingStartEndAction(e.start, e.end));
+    // setuserSelectionDetails(e);
+    handleShow();
+  };
+  return (
+    <>
+      <Calendar
+        localizer={localizer}
+        events={allEvents}
+        titleAccessor="agenda"
+        startAccessor="startTime"
+        endAccessor="endTime"
+        selectable={'ignoreEvents'}
+        onSelectSlot={(e) => {
+          handleCreateEvent(e);
+        }}
+        onSelectEvent={(event) => alert(event.title)}
+        defaultView="month"
+        views={['month', 'week', 'day']}
+        // Determines the selectable time increments in week and day views
+        step={30}
+        // timeslots={30}
+        min={getLowerBoundDate()} // 8.00 AM
+        max={getUpperBoundDate()} // Max will be 6.00 PM!
+      />
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <Button onClick={handleNextPage}> Next</Button>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
+            <Button onClick={handleCancelForm}> Cancel</Button>
+          </div>
+        </div>
+      </div>
+      <CreateBookingModal
+        userSelectionDetails={userSelectionDetails}
+        show={show}
+        handleClose={handleClose}
+      />
+    </>
+  );
+}
