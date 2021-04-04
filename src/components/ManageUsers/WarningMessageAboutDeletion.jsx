0175d34c5bec4629e7e04d9c2f0b5e0e372071a5
jeremylim91 <jeremylim_91@live.com>
2021-04-04 09:40:52 +0800

    created modal to warn user before he deletes another usr

---
+++ src/components/ManageUsers/WarningMessageAboutDeletion.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {Container, Row, Col} from 'react-bootstrap';
+
+export default function WarningMessageAboutDeletion({users}) {
+  console.log(users);
+  const listUsers = users.map((elem) => <li key={elem.id}> {elem.name}</li>);
+
+  return (
+    <div>
+      <Container>
+        <Row>
+          <Col>
+            You are about to delete the following users:
+            <ol>{listUsers}</ol>
+            Are you sure you want to proceed?
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+}
