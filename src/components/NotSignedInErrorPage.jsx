9a439457e7c49a65b4928687392c297f8e143a36
jeremylim91 <jeremylim_91@live.com>
2021-04-04 09:37:47 +0800

    created a view to tell users they need to sign in before proceeding further

---
+++ src/components/NotSignedInErrorPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function NotSignedInErrorPage() {
+  return (
+    <div>
+      Looks like you have not signed in. Please sign in to access the page.
+    </div>
+  );
+}
