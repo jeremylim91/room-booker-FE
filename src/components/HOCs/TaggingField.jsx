0175aaf43515776a0f02919acb9b1e5ab6dc40ce
jeremylim91 <jeremylim_91@live.com>
2021-04-07 01:40:12 +0800

    created the tagging field component that is designed to be reused in both dashboard and create booking

---
+++ src/components/HOCs/TaggingField.jsx
@@ -0,0 +1,54 @@
+import React, {useContext} from 'react';
+import ReactTags from 'react-tag-autocomplete';
+import {Form} from 'react-bootstrap';
+import {
+  CreateBookingContext,
+  updateAttendeesAction,
+} from '../../createBookingStore.jsx';
+
+// const {dispatchBookingForm} = useContext(CreateBookingContext);
+
+export default function TaggingField({
+  labelProp,
+  suggestionsProp,
+  tagsProp,
+  setTagsProp,
+  dispatchBookingForm,
+}) {
+  console.log(`tagsProp in the modal is:`);
+  console.log(tagsProp);
+
+  const onAddition = (tag) => {
+    const newTagsProp = [...tagsProp, tag];
+    // Save to createBoookingStore via the dispatch fn
+    if (dispatchBookingForm)
+      dispatchBookingForm(updateAttendeesAction(newTagsProp));
+    setTagsProp(newTagsProp);
+  };
+
+  const onDelete = (i) => {
+    let newTagsProp = [...tagsProp];
+    newTagsProp = newTagsProp.slice(0);
+    newTagsProp.splice(i, 1);
+    // Save to createBoookingStore via the dispatch fn
+    if (dispatchBookingForm)
+      dispatchBookingForm(updateAttendeesAction(newTagsProp));
+
+    setTagsProp(newTagsProp);
+  };
+
+  return (
+    <>
+      <Form.Group>
+        <Form.Label>{labelProp}:</Form.Label>
+        <ReactTags
+          suggestions={suggestionsProp}
+          tags={tagsProp}
+          onDelete={onDelete}
+          onAddition={onAddition}
+          noSuggestionsText="User not found; Please enter valid user"
+        />
+      </Form.Group>
+    </>
+  );
+}
