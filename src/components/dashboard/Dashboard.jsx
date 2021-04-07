35b9ae2aab7d3b5761b23585d37d66e756989517
jeremylim91 <jeremylim_91@live.com>
2021-04-07 08:45:46 +0800

    added code to allow users to see their own mtgs in a diff color

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -1,19 +1,33 @@
 import React, {useContext, useState, useEffect} from 'react';
-// import CreateBookingModal from './CreateBookingModal.jsx';
-import Button from 'react-bootstrap/Button';
-import {RoomBookerContext, getAllEventsByUserId} from '../../store.jsx';
-import {Calendar, momentLocalizer} from 'react-big-calendar';
+import {Button, Container, Row, Col, Dropdown} from 'react-bootstrap';
+import {
+  RoomBookerContext,
+  getCalendarEventsDisplay,
+  dashboardFilters,
+} from '../../store.jsx';
+import {Calendar, momentLocalizer, Views} from 'react-big-calendar';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import moment from 'moment';
 import withDragAndDrop from 'react-big-calendar/lib/addons/dragAndDrop';
 import '../../styles/calendarStyles.scss';
 import 'react-big-calendar/lib/addons/dragAndDrop/styles.css';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
-
+import EventInfoModal from './EventInfoModal.jsx';
 import {
   getLowerBoundDate,
   getUpperBoundDate,
 } from '../../utils/calendarRelatedFns.mjs';
+import {getEvents} from './events.jsx';
+import {MdSettingsInputAntenna} from 'react-icons/md';
+import {MyToolbar} from './MyToolbar.jsx';
+import {getUserIdFromCookie} from '../../utils/cookieRelatedFns.mjs';
+
+console.log(`Views is:`);
+console.log(Views);
+
+let allViews = Object.keys(Views).map((k) => Views[k]);
+console.log(`allViews is:`);
+console.log(allViews);
 
 // Setup the localizer by providing the moment (or globalize) Object
 // to the correct localizer.
@@ -25,6 +39,7 @@ export default function Dashboard() {
   const {store, dispatchBookingForm} = useContext(RoomBookerContext);
 
   const {loggedInUserId} = store;
+  const {ALL_MEETINGS, MY_MEETINGS, ANY, ONYX, PARK, HALO} = dashboardFilters;
 
   // logic that tells app to get roomId if alr in local storage, else get it from global state
 
@@ -32,9 +47,23 @@ export default function Dashboard() {
   // manage modal display
   const [show, setShow] = useState(false);
   // display all meetings taking place in the room specified by user
-  const [allEventsByUserId, setAllEventsByUserId] = useState([]);
+  // const [allEventsByUserId, setAllEventsByUserId] = useState([]);
+  const [calendarEventDisplay, setCalendarEventDisplay] = useState([]);
+
+  const [meetingFilter, setMeetingFilter] = useState(MY_MEETINGS);
+  const [resourceFilter, setResourceFilter] = useState(ANY);
+
+  // query db to get data to populate calendar; should hve 1. all meetings, 2. all of user's mtgs
+  useEffect(() => {
+    getCalendarEventsDisplay(setCalendarEventDisplay);
+  }, []);
+  console.log(calendarEventDisplay);
+
   // Meeting details that the user inputs
   const [userSelectionDetails, setuserSelectionDetails] = useState({});
+
+  // set a state that stores the event from the user's selection
+  const [event, setEvent] = useState('');
   // ==================================================
 
   // =======set states to manage modal open/close======
@@ -43,71 +72,151 @@ export default function Dashboard() {
   // =================================================
 
   // load all events for current user
-  useEffect(() => {
-    console.log(`running use effect in dashboard`);
-    getAllEventsByUserId(setAllEventsByUserId);
-  }, []);
-  console.log(`allEventsByUserId is:`);
-  console.log(allEventsByUserId);
+  // useEffect(() => {
+  //   console.log(`running use effect in dashboard`);
+  //   getAllEventsByUserId(setAllEventsByUserId);
+  // }, []);
 
-  // this fn runs whenever a user selects a timeslot on the calendar
-  // const handleCreateEvent = (e) => {
-  //   console.log(e.start);
-  //   dispatchBookingForm(updateMeetingStartEndAction(e.start, e.end));
-  //   // setuserSelectionDetails(e);
-  //   handleShow();
+  const handleSelectEvent = (e) => {
+    setEvent(e);
+    handleShow();
+  };
+
+  // const ManageFilterDropdown = (filter, dropdownOptions) => {
+  //   dropdownOptions.forEach((elem, idx) => {
+  //     if (elem === filter) {
+  //       dropdownOptions.splice(idx, 1);
+  //     }
+  //   });
+  //   return dropdownOptions.map((elem) => <Dropdown.Item>{elem}</Dropdown.Item>);
   // };
+
+  const manageDropdownBtnClick = (mode, relevantSetState) => {
+    relevantSetState(mode);
+  };
+
+  const ManageFilterDropdown = (filter, dropdownOptions, relevantSetState) =>
+    dropdownOptions.map((elem) =>
+      elem !== filter ? (
+        <Dropdown.Item
+          onClick={(e) => manageDropdownBtnClick(elem, relevantSetState)}>
+          {elem}
+        </Dropdown.Item>
+      ) : (
+        ''
+      )
+    );
+
+  const styleMyEvents = (e, startTime, endTime) => {
+    if (e.userId === getUserIdFromCookie()) {
+      const style = {
+        backgroundColor: 'orange',
+        color: 'white',
+        display: 'block',
+        opacity: 0.8,
+      };
+      return {
+        style: style,
+      };
+    }
+  };
+
+  const events = getEvents(calendarEventDisplay, meetingFilter, resourceFilter);
+
   return (
     <>
-      <Calendar
-        localizer={localizer}
-        events={
-          // check if the state has been set with the user's dates
-          Object.keys(allEventsByUserId).length > 0
-            ? [
-                ...allEventsByUserId.userMeetings,
-                ...allEventsByUserId.bookedByUser,
-              ]
-            : []
+      <Container fluid className="ml-2, mr-2, mt-2">
+        <Row>
+          <Col>Filters:</Col>
+          <Col>
+            <Dropdown>
+              <Dropdown.Toggle variant="outline-primary" id="dropdown-basic">
+                {meetingFilter}
+              </Dropdown.Toggle>
 
-          //   {
-          //   ...allEventsByUserId.userMeetings,
-          //   ...allEventsByUserId.bookedByUser,
-          // }
-        }
-        titleAccessor="agenda"
-        startAccessor="startTime"
-        endAccessor="endTime"
-        // selectable={'ignoreEvents'}
-        // onSelectSlot={(e) => {
-        //   handleCreateEvent(e);
-        // }}
-        onSelectEvent={(event) => alert(event.title)}
-        defaultView="month"
-        views={['month', 'week', 'day']}
-        // Determines the selectable time increments in week and day views
-        step={30}
-        // timeslots={30}
-        min={getLowerBoundDate()} // set at 8.00 AM
-        max={getUpperBoundDate()} // Max will be 6.00 PM
-      />
-      <div className="container">
-        <div className="row">
-          <div className="col">
-            <Button onClick={() => {}}> Next</Button>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col">
-            <Button onClick={() => {}}> Cancel</Button>
-          </div>
-        </div>
-      </div>
-      {/* <CreateBookingModal
-        userSelectionDetails={userSelectionDetails}
-        show={show}
-        handleClose={handleClose}
-      /> */}
+              <Dropdown.Menu>
+                {ManageFilterDropdown(
+                  meetingFilter,
+                  [MY_MEETINGS, ALL_MEETINGS],
+                  setMeetingFilter
+                )}
+              </Dropdown.Menu>
+            </Dropdown>
+          </Col>
+          <Col>
+            <Dropdown>
+              <Dropdown.Toggle variant="outline-primary" id="dropdown-basic">
+                {resourceFilter}
+              </Dropdown.Toggle>
+
+              <Dropdown.Menu>
+                {ManageFilterDropdown(
+                  resourceFilter,
+                  [ANY, ONYX, PARK, HALO],
+                  setResourceFilter
+                )}
+              </Dropdown.Menu>
+            </Dropdown>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Calendar
+              localizer={localizer}
+              events={events}
+              style={{height: '100vh'}}
+              titleAccessor="agenda"
+              startAccessor="startTime"
+              endAccessor="endTime"
+              onSelectEvent={handleSelectEvent}
+              toolbar={MyToolbar}
+              // selectable={'ignoreEvents'}
+              // onSelectSlot={(e) => {
+              //   handleCreateEvent(e);
+              // }}
+              defaultView="month"
+              views={['month', 'week', 'day']}
+              // Determines the selectable time increments in week and day views
+              step={30}
+              // timeslots={30}
+
+              eventPropGetter={styleMyEvents}
+              min={getLowerBoundDate()} // set at 8.00 AM
+              max={getUpperBoundDate()} // Max will be 6.00 PM
+            />
+          </Col>
+        </Row>
+        <Row className="mt-3">
+          <Col xs={12}>
+            <h3> Upcoming this week:</h3>
+          </Col>
+          <Col xs={12}>
+            <Calendar
+              localizer={localizer}
+              events={events}
+              style={{height: '100vh'}}
+              titleAccessor="agenda"
+              startAccessor="startTime"
+              endAccessor="endTime"
+              onSelectEvent={handleSelectEvent}
+              toolbar={false}
+              defaultView="agenda"
+              views={['agenda']}
+              step={30}
+              // timeslots={30}
+              min={getLowerBoundDate()} // set at 8.00 AM
+              max={getUpperBoundDate()} // Max will be 6.00 PM
+            />
+          </Col>
+        </Row>
+      </Container>
+
+      {show && (
+        <EventInfoModal
+          show={show}
+          handleClose={handleClose}
+          event={event}></EventInfoModal>
+      )}
     </>
   );
 }
