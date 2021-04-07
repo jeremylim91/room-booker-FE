b6c5aa768f531a865b8677af380fc850e79b150a
jeremylim91 <jeremylim_91@live.com>
2021-04-07 08:46:22 +0800

    added code so tt user can check cookies for isAdmin

--- src/utils/cookieRelatedFns.mjs
+++ src/utils/cookieRelatedFns.mjs
@@ -9,6 +9,7 @@ export const getUsernameFromCookie = () => {
   return null;
 };
 
+// function to get userid; assumes that userId was the second cookie that was set when user logged in
 export const getUserIdFromCookie = () => {
   if (document.cookie) {
     const splitCookieVal = document.cookie.split(' ');
@@ -16,6 +17,20 @@ export const getUserIdFromCookie = () => {
     const userIdEndPos = splitCookieVal[1].indexOf(';');
     const loggedInUserId = splitCookieVal[1].substring(userIdStartPos, userIdEndPos);
     return Number(loggedInUserId);
+  }
+  return null;
+};
+
+
+// function to get userid; assumes that cookie is the last (or idx 3)  cookie that was set when user logged n 
+export const getIsAdminFromCookie = () => {
+  if (document.cookie) {
+    const splitCookieVal = document.cookie.split(' ');
+    console.log(splitCookieVal)
+    const isAdminStartPos = splitCookieVal[3].indexOf('=') + 1;
+    const isAdminEndingPos = isAdminStartPos+11;
+    const isAdminVal = splitCookieVal[3].substring(isAdminStartPos, isAdminEndingPos);
+    return isAdminVal==='true'
   }
   return null;
 };
