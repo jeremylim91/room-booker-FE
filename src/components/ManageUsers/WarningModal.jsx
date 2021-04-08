a2422472f7a74ddd22b71c34435892f6811e7406
jeremylim91 <jeremylim_91@live.com>
2021-04-08 09:13:02 +0800

    re-created WarningModal as a separate componente becos it was not needed as a HOC

---
+++ src/components/ManageUsers/WarningModal.jsx
@@ -0,0 +1,51 @@
+import {Modal, Container, Row, Col, Button} from 'react-bootstrap';
+import React from 'react';
+import {deleteUser} from '../../store.jsx';
+
+export default function WarningModal({
+  show,
+  handleClose,
+  taggedUsers,
+  setTaggedUsers,
+  forceRender,
+}) {
+  const handleDeleteUser = () => {
+    const newTaggedUsers = [...taggedUsers];
+    deleteUser(newTaggedUsers, setTaggedUsers);
+    handleClose();
+  };
+  const displayAttendees = () => {
+    return taggedUsers.map((taggedUser) => (
+      <li key={taggedUser.id}> {taggedUser.name}</li>
+    ));
+  };
+
+  return (
+    <>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Attention!</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Container>
+            <Row>
+              <Col>The following users will be removed:</Col>
+            </Row>
+            <Row>
+              <Col>{displayAttendees()}</Col>
+            </Row>
+          </Container>
+          {/* <ChildComponent users={taggedUsers} /> */}
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="outline-danger" onClick={handleDeleteUser}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}
