5995bc3c0f04df118ba7d70f95e90916fbbb994a
jeremylim91 <jeremylim_91@live.com>
2021-04-08 17:34:00 +0800

    added classnames and css

--- src/components/HomePage/Amenities.jsx
+++ src/components/HomePage/Amenities.jsx
@@ -1,54 +1,61 @@
-import React from 'react'
-import Title from './Title.jsx'
-import { FaConciergeBell } from "react-icons/fa";
-import { MdLocalDrink} from "react-icons/md";
-import { AiOutlineFundProjectionScreen} from "react-icons/ai";
-
+import React from 'react';
+import {Container, Row, Col} from 'react-bootstrap';
+import Title from './Title.jsx';
+import {FaConciergeBell} from 'react-icons/fa';
+import {MdLocalDrink} from 'react-icons/md';
+import {AiOutlineFundProjectionScreen} from 'react-icons/ai';
+import '../../styles/HomePage.scss';
 
 export default function Amenities() {
-  
-  const amenities= [
-      {
-        icon: <FaConciergeBell />,
-        title: "Concierge Service",
-        info:
-          `Our on-site concierge provides the following services:
+  const amenities = [
+    {
+      icon: <FaConciergeBell />,
+      title: 'Concierge Service',
+      info: `Our on-site concierge provides the following services:
           1. Ordering refreshments/meals for meetings
           2. Troubleshooting IT devices (e.g. presentation aids)
-          `
-      },
-      {
-        icon: <AiOutlineFundProjectionScreen />,
-        title: "Presentation aids",
-        info:
-          `All our meeting rooms are equipped with a range of digital and analogue presentation aids, ranging from projectors to whiteboards.`
-      },
-      {
-        icon: <MdLocalDrink />,
-        title: "Light Refreshments",
-        info:
-          "Our meeting rooms come with access to fresh water and a variety of teas to energise your meetings"
-      },
-    ]
-  const DisplayAmenities= ()=>{
-    return(
-    amenities.map((amenity, idx)=>{
-      return(
-        <article key={idx} className='amenities'>
-          <span>{amenity.icon}</span>
-          <h6>{amenity.title}</h6>
-          <p>{amenity.info}</p>
-        </article>
-      )
-    })
-    )}
-  
+          `,
+    },
+    {
+      icon: <AiOutlineFundProjectionScreen />,
+      title: 'Presentation aids',
+      info: `All our meeting rooms are equipped with a range of digital and analogue presentation aids, ranging from projectors to whiteboards.`,
+    },
+    {
+      icon: <MdLocalDrink />,
+      title: 'Light Refreshments',
+      info:
+        'Our meeting rooms come with access to fresh water and a variety of teas to energise your meetings',
+    },
+  ];
+  const DisplayAmenities = () => {
+    return amenities.map((amenity, idx) => {
+      return (
+        <Col className="amenities-col">
+          <article key={idx} className="amenities">
+            <span>{amenity.icon}</span>
+            <h6>{amenity.title}</h6>
+            <p>{amenity.info}</p>
+          </article>
+        </Col>
+      );
+    });
+  };
+
   return (
     <section className="amenities">
-        <Title title="Amenities" />
-        <div className="amenities-center">
-          <DisplayAmenities/>
-        </div>
-      </section>
-  )
+      <Container>
+        <Row>
+          <Col className="mt-4">
+            <Title title="Amenities" />
+          </Col>
+        </Row>
+        <Row>
+          {/* <div className="amenities-center"> */}
+          <DisplayAmenities className="display-Amentities" />
+          {/* </div> */}
+        </Row>
+      </Container>
+    </section>
+  );
 }
--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,25 +1,24 @@
-import React from 'react'
-import Amenities from './Amenities.jsx'
-import Hero from './Hero.jsx'
-import Banner from './Banner.jsx'
-import Button from 'react-bootstrap/Button'
-import {Link} from 'react-router-dom'
-import '../../styles/HomePage.scss'
-
+import React from 'react';
+import '../../styles/HomePage.scss';
+import Amenities from './Amenities.jsx';
+import Hero from './Hero.jsx';
+import Banner from './Banner.jsx';
+import Button from 'react-bootstrap/Button';
+import {Link} from 'react-router-dom';
 
 export default function HomePage() {
   return (
-  <>
-    <Hero>
-      <Banner
-      title="Best-In-Class Rooms"
-      subtitle= "“This is the best meeting room I've ever been in”"
-      >
-      <Link to="/createBooking" className="btn-primary"><Button> BOOK NOW </Button></Link>
-        
-      </Banner>
-    </Hero>
-    <Amenities/>
-  </>
-  )
+    <>
+      <Hero>
+        <Banner
+          title="That Meeting Place"
+          subtitle="Contemporary meetings rooms for modern needs">
+          <Link to="/createBooking" className="btn-primary">
+            <Button> BOOK NOW </Button>
+          </Link>
+        </Banner>
+      </Hero>
+      <Amenities />
+    </>
+  );
 }
