240b59cb07eda9b341b1019ca36e12f54a1acb62
jeremylim91 <jeremylim_91@live.com>
2021-04-08 21:58:58 +0800

    removed unused components from room booking process

--- src/components/NotSignedInErrorPage.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function NotSignedInErrorPage() {
-  return (
-    <div>
-      Looks like you have not signed in. Please sign in to access the page.
-    </div>
-  );
-}
